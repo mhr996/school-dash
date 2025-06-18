27ad7124013826ce3eea761559328a309e8fb375
zacker101 <tombson.m@gmail.com>
2025-06-19 01:38:30 +0300

    updates

--- components/icon/icon-receipt.tsx
+++ components/icon/icon-receipt.tsx
@@ -7,20 +7,10 @@ interface IconReceiptProps {
 const IconReceipt: FC<IconReceiptProps> = ({ className }) => {
     return (
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-            <path
-                d="M9 12L11 14L15 10"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M21 5L16 2L10 2L3 5V19L10 22L16 22L21 19V5Z"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
+            <path d="M3 3V21L6 18L9 21L12 18L15 21L18 18L21 21V3H3Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M7 8H17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M7 12H17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M7 16H12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
 };
